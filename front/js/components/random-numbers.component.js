a2f900e807ac61608305f74c9a3a549a69192191
Anna <trzeciakanna@hotmail.com>
2018-06-14 14:03:34 +0200

    show random numbers

---
+++ front/js/components/random-numbers.component.js
@@ -0,0 +1,37 @@
+function RandomNumbers(selector) {
+  Component.call(this, selector);
+  this.numbers = [];
+}
+
+RandomNumbers.prototype = Object.create(Component.prototype);
+RandomNumbers.constructor = RandomNumbers;
+
+RandomNumbers.prototype.init = function() {
+  const self = this;
+
+  axios.get('http://localhost:3000/random-numbers')
+    .then(function(response) {
+      self.numbers = response.data.data.map(function(number) {
+        return {
+          id: number
+        }
+      });
+
+      self.render();
+    })
+    .catch(function(error) {
+      console.error(error);
+    });
+};
+
+RandomNumbers.prototype.render = function() {
+  const container = this.getDOMElement();
+
+  this.numbers.forEach(function(number) {
+      const listElement = document.createElement('li');
+      listElement.classList.add('list-group-item');
+      listElement.innerHTML = number.id;
+
+      container.appendChild(listElement);
+  });
+};
--- front/js/main.js
+++ front/js/main.js
@@ -1,2 +1,4 @@
+const randomNumbers = new RandomNumbers('#random-numbers');
+randomNumbers.init();
 const ranking = new Ranking('#numbers-ranking');
 ranking.init();
